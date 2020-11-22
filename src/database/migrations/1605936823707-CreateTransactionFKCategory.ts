37ef7b1a90a6518aff8a4f31cb502e82a9e10ccc
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2020-11-21 23:49:50 -0300

    :sparkles: Chalenge Done!

---
+++ src/database/migrations/1605936823707-CreateTransactionFKCategory.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateTransactionFKCategory1605936823707
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategory_FK',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory_FK');
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
